cb251a3bd72597427fbddc60920b7c6ca54875e5
EsotericZ <cjsand03@gmail.com>
2023-04-22 20:48:40 -0600

    more stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,52 @@
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Navbar from './components/Navbar';
+import Single from './pages/Single';
+import Write from './pages/Write';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  { 
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      { 
+        path: '/post/:id',
+        element: <Single />,
+      },
+      { 
+        path: '/Write',
+        element: <Write />,
+      },
+    ]
+  },
+  { 
+    path: '/Login',
+    element: <Login />,
+  },
+])
+
 function App() {
   return (
-    <div>
-      Hello World
+    <div classNae="app">
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }

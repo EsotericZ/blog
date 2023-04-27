5e3bf7d01b7a6bbb2ceba0de14464ee8267e51a8
EsotericZ <cjsand03@gmail.com>
2023-04-27 12:38:33 -0600

    single page

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,7 +1,28 @@
+import { Link } from 'react-router-dom';
+import Delete from '../img/delete.png';
+import Edit from '../img/edit.png';
+import Profile from '../img/profile.png';
+
 const Single = () => {
     return (
-        <div>
-            Single
+        <div className='single'>
+            <div className='content'>
+                <img />
+                <div className='user'>
+                    <img src={Profile} alt='' />
+                </div>
+                <div className='info'>
+                    <span>CJ</span>
+                    <p>Posted</p>
+                </div>
+                <div className='edit'>
+                    <Link to={`/write?edit=2`}>
+                        <img src={Edit} alt='' />
+                    </Link>
+                    <img src={Delete} alt='' />
+                </div>
+            </div>
+            <div className='menu'></div>
         </div>
     )
 }
